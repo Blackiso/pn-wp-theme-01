cdfbb021db729397da964fc42001ed06b3e20ffc
Blackiso <ismailboy00@gmail.com>
2020-02-29 23:31:21 +0100

    added support for menus and header / footer

--- functions.php
+++ functions.php
@@ -3,6 +3,8 @@
 	define('TEMPLATE_DIR', get_template_directory());
 	define('TEMPLATE_URI', get_template_directory_uri());
 
+	// Theme features:
+	add_theme_support('menus');
 
 	function register_styles() {
 		$files = scandir(TEMPLATE_DIR.'/css');
@@ -27,6 +29,24 @@
 		wp_enqueue_script('main', get_template_directory_uri().'/js/main.js', false, '1', true);
 	}
 
+	function menu_custom_li_classes($classes) {
+		$classes[] = 'nav-item';
+		return $classes;
+	}
 
+	function menu_custom_a_classes($atts) {
+		$atts['class'] = 'nav-link';
+		return $atts;
+	}
+
+	function register_menus() {
+		register_nav_menus(['main-menu' => __('Main menu')]);
+	}
+
+
+	// Wordpress hooks:
+	add_action('after_setup_theme', 'register_menus');
 	add_action('wp_enqueue_scripts', 'register_scripts');
 	add_action('wp_enqueue_scripts', 'register_styles');
+	add_filter('nav_menu_css_class', 'menu_custom_li_classes');
+	add_filter('nav_menu_link_attributes', 'menu_custom_a_classes');
