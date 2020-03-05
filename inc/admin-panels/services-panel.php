25cbf8133ce0b0488655378cc79d48484a985852
Blackiso <ismailboy00@gmail.com>
2020-03-05 19:14:35 +0100

    Improved panel UI for services page

--- inc/admin-panels/services-panel.php
+++ inc/admin-panels/services-panel.php
@@ -1,24 +1,29 @@
+<input type="hidden" name="current_meta_box" value="<?php echo $current_meta_box ?>">
 <div class="snipp-section-options">
 	<?php 
-		create_text_input('Services Section Title', 'service-section', 'title', 'Enter services section title'); 
+		create_text_input('Services Section Title', 'title', 'Enter services section title', 
+			$current_values['services-section']['title'], 'services-section'); 
 		create_text_input(
-			'Services Section Description', 'service-section', 'des', 
-			'Enter services section description', null, 'textarea'
+			'Services Section Description', 'des', 'Enter services section description', 
+			$current_values['services-section']['des'], 'services-section', null, 'textarea'
 		); 
 	?>
 </div>
 <div class="snipp-services-block">
 	
-	<?php for ($i = 0; $i < 3; $i++): ?>
+	<?php for ($i = 0; $i < 4; $i++): ?>
 
 	<div class="snipp-single-service">
-		<span class="snipp-single-service-title">Service #1</span>
+		<span class="snipp-single-service-title">#<?php echo __('Service').$i+1; ?></span>
 		<div>
 		<?php 
-			create_text_input('Service Title', 'services', 'service-title', 'Enter service title'); 
 			create_text_input(
-				'Service Description', 'services', 'service-des', 
-				'Enter service description', null, 'textarea'
+				'Service Title', 'title', 'Enter service title', $current_values['services'][$i]['title'],
+				 null, 'services', 'input', $i
+			); 
+			create_text_input(
+				'Service Description', 'des', 'Enter service description', $current_values['services'][$i]['des'], 
+				null, 'services', 'textarea', $i
 			); 
 		?>
 		</div>
