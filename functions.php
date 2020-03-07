6c2937f9b3fa688fd19b89f909c024039081a195
ZakariaBenali <ZakariaBenali@users.noreply.github.com>
2020-03-07 01:10:59 +0100

    enqueuing scripts and styles

--- functions.php
+++ functions.php
@@ -1,31 +1,37 @@
-
 <?php
-define("TEMPLATE_DIR", get_template_directory());
-    function wp_style_functions() {
-        wp_enqueue_style('main', get_template_directory_uri() . '/css/style.css',false,'1','all');
-        wp_enqueue_style('animate', get_template_directory_uri() . '/css/animate.css',false,'1','all');
-        wp_enqueue_style('aos', get_template_directory_uri() . '/css/aos.css',false,'1','all');
-        wp_enqueue_style('icomoon', get_template_directory_uri() . '/css/icomoon.css',false,'1','all');
-        wp_enqueue_style('owl', get_template_directory_uri() . '/css/owl.carousel.min.css',false,'1','all');
-        wp_enqueue_style('bootstrap-datepicker', get_template_directory_uri() . '/css/bootstrap-datepicker.css',false,'1','all');
-        wp_enqueue_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css',false,'1','all');
-        wp_enqueue_style('flaticon', get_template_directory_uri() . '/css/flaticon.css',false,'1','all');
+    function locate_stylesheets() {
+        $theme_dir = get_template_directory();
+        $theme_uri = get_template_directory_uri();
+        $cssfiles = glob($theme_dir."/css/*css");
+        foreach($cssfiles as $file) {
+            $index = strpos($file,"/css/");
+            $fileName = substr($file,$index + strlen("/css/"));
+            $filePath = $theme_uri . "/css/" . $fileName;
+            if ($fileName =="style.css") {
+                wp_enqueue_style( $fileName ,$filePath, array("bootstrap.min.css"), '1.1', 'all');
+            }
+            wp_enqueue_style( $fileName ,$filePath, array(), '1.1', 'all');
+        }
     }
-    
-    add_action('wp_enqueue_scripts','wp_style_functions');
 
 
-    function register_scripts() {
-		$files = scandir(TEMPLATE_DIR.'/js');
-		foreach ($files as $file) {
-			$dependecy = [];
-			if ($file !== 'jquery.min.js') array_push($dependecy, 'jquery.min.js');
+    function locate_scripts() {
+        $theme_dir = get_template_directory();
+        $theme_uri = get_template_directory_uri();
+        wp_enqueue_script( 'jquery.min.js', $theme_uri . '/js/jquery.min.js', false, '1', 'true');
+        $jsfiles = glob($theme_dir."/js/*js");
+        foreach($jsfiles as $file) {
+            $index = strpos($file,"/js/");
+            $fileName = substr($file,$index + strlen("/js/"));
+            $filePath = $theme_uri . "/js/" . $fileName;
+            if ($fileName !="main.js" && $fileName != "jquery.min.js" && $fileName != "particle.js") {
+                wp_enqueue_script( $fileName , $filePath, false, '1', 'true');
+            }
+        }
+        wp_enqueue_script( 'particle.js', $theme_uri . '/js/particle.js', false, '1', 'true');
+        wp_enqueue_script( 'main.js', $theme_uri . '/js/main.js', array("jquery.min.js"), '1', 'true');
+    }
 
-			if (strpos($file, '.js') !== false && $file !== 'main.js') {
-				wp_enqueue_script($file, get_template_directory_uri().'/js/'.$file, $dependecy, '1', true);
-			}
-		}
-		wp_enqueue_script('main', get_template_directory_uri().'/js/main.js', false, '1', true);
-	}
-    add_action('wp_enqueue_scripts','register_scripts');
+    add_action('wp_enqueue_scripts','locate_stylesheets');
+    add_action('wp_enqueue_scripts',"locate_scripts");
 ?>
